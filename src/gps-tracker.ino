3bbb0b83f10c2c52aa3b7e359e3727795fca31ef
Adam DeMuri <adam.demuri@gmail.com>
2023-07-13 18:59:41 -0600

    Add basic GPS functionality

--- src/gps-tracker.ino
+++ src/gps-tracker.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_LIS2MDL.h>
 #include <Adafruit_LTR329_LTR303.h>
 #include <Arduino.h>
+#include <TinyGPSPlus.h>
 
 #include <algorithm>
 
@@ -18,6 +19,7 @@ constexpr int kSda = PA10;
 
 Adafruit_LIS2MDL compass;
 Adafruit_LTR303 light_sensor;
+TinyGPSPlus gps;
 
 void FatalError() {
   bool on = false;
@@ -79,6 +81,9 @@ void setup() {
   // light_sensor.setGain(LTR3XX_GAIN_1);
   // light_sensor.setIntegrationTime(LTR3XX_INTEGTIME_400);
   // light_sensor.setMeasurementRate(LTR3XX_MEASRATE_500);
+
+  Serial3.begin(9600);
+  delay(1000);
 }
 
 void DumpCompassMinMax() {
@@ -107,8 +112,8 @@ void DumpCompassMinMax() {
 void DumpCompassHeading() {
   sensors_event_t event;
   compass.getEvent(&event);
-  int16_t x = compass.raw.x -147 + 297;
-  int16_t y = compass.raw.y -76.5 + 151.0;
+  int16_t x = compass.raw.x - 147 + 297;
+  int16_t y = compass.raw.y - 76.5 + 151.0;
   int16_t z = compass.raw.z + 6 - 19.5;
   float heading = (atan2(y, x) * 180) / 3.141593;
 
@@ -125,14 +130,15 @@ void DumpCompassHeading() {
 void DumpCompassValuesForCalibration() {
   sensors_event_t event;
   compass.getEvent(&event);
-  Serial2.printf("Uni:0,0,0,0,0,0,%d,%d,%d\n", compass.raw.x, compass.raw.y, compass.raw.z);
+  Serial2.printf("Uni:0,0,0,0,0,0,%d,%d,%d\n", compass.raw.x, compass.raw.y,
+                 compass.raw.z);
 }
 
 void DumpLightSensor() {
   bool valid;
   uint16_t visible_plus_ir, infrared;
 
-   if (light_sensor.newDataAvailable()) {
+  if (light_sensor.newDataAvailable()) {
     valid = light_sensor.readBothChannels(visible_plus_ir, infrared);
     if (valid) {
       Serial.print("CH0 Visible + IR: ");
@@ -145,7 +151,45 @@ void DumpLightSensor() {
   }
 }
 
+void GetGpsFirmwareVersion() {
+  Serial3.println("$PQVERNO,R*3F");
+  // TODO: read a response
+}
+
+void DumpGpsOutput() {
+  while (Serial3.available()) {
+    char in = Serial3.read();
+    Serial2.print(in);
+  }
+}
+
+void DumpGpsLocation() {
+  while (Serial3.available()) {
+    gps.encode(Serial3.read());
+  }
+
+  if (gps.satellites.isValid()) {
+    Serial2.printf("Found %d satellites\n", gps.satellites.value());
+  } else {
+    Serial2.println("Found no satellites\n");
+  }
+
+  if (gps.location.isValid()) {
+    Serial2.printf("Location: %3.3f, %3.3f\n", gps.location.lat(),
+                   gps.location.lng());
+  } else {
+    Serial2.println("No valid GPS location");
+  }
+
+  Serial2.println();
+}
+
+uint32_t send_at = 0;
+
 void loop() {
-  delay(100);
-  DumpCompassHeading();
+  // delay(100);
+  // DumpCompassHeading();
+
+  DumpGpsLocation();
+  delay(1000);
 }
